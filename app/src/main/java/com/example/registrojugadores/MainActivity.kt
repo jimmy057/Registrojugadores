336b8c8c2537a4671f5dbf047efd718e0e3a9023
jimmy057 <jimmypolanco2540@gmail.com>
2025-09-06 15:47:21 -0400

    MainActivity modificado

--- app/src/main/java/com/example/registrojugadores/MainActivity.kt
+++ app/src/main/java/com/example/registrojugadores/MainActivity.kt
@@ -4,44 +4,56 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import com.example.registrojugadores.presentation.tareas.edit.EditJugadorScreen
+import com.example.registrojugadores.presentation.tareas.list.ListJugadorScreen
 import com.example.registrojugadores.ui.theme.RegistroJugadoresTheme
 
+
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             RegistroJugadoresTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold (
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(
+                                    text = "Lista de jugadores",
+                                    style = MaterialTheme.typography.titleLarge
+                                )
+                            },
+                        )
+                    },
+                ){
+                    Column (
+                        modifier = Modifier.fillMaxSize()
+                            .padding(it)
+                    ) {
+                        EditJugadorScreen()
+                        ListJugadorScreen()
+                    }
+
                 }
+
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RegistroJugadoresTheme {
-        Greeting("Android")
     }
 }
