383d8ef151ee04d803630603f374c557d1d746f7
jimmy057 <jimmypolanco2540@gmail.com>
2025-09-17 14:06:31 -0400

    agregue partidas.kt

---
+++ app/src/main/java/com/example/registrojugadores/data/local/entities/partidas.kt
@@ -0,0 +1,15 @@
+package com.example.registrojugadores.data.local.entities
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "partidas")
+data class PartidaEntity(
+    @PrimaryKey(autoGenerate = true) val partidaId: Int = 0,
+    val fecha: String,
+    val jugador1Id: Int,
+    val jugador2Id: Int,
+    val ganadorId: Int?,
+    val esFinalizada: Boolean
+)
