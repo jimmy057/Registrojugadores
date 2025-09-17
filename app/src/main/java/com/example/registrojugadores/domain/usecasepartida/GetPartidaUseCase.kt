b1e03e9789263c7fff34fd6674ad99e9166da6aa
jimmy057 <jimmypolanco2540@gmail.com>
2025-09-17 13:58:32 -0400

    agregue GetPartidaUseCase.kt

---
+++ app/src/main/java/com/example/registrojugadores/domain/usecasepartida/GetPartidaUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.registrojugadores.domain.usecasepartida
+
+import com.example.registrojugadores.domain.model.Partida
+import com.example.registrojugadores.domain.repository.PartidaRepository
+import javax.inject.Inject
+
+class GetPartidaUseCase @Inject constructor(
+    private val repository: PartidaRepository
+) {
+    suspend operator fun invoke(id: Int): Partida? {
+        return repository.getPartidaById(id)
+    }
+}
