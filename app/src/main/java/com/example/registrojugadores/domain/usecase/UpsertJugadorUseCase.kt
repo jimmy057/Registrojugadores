228733bef55197b929822cbeccab5ad22e4f11d0
jimmy057 <jimmypolanco2540@gmail.com>
2025-09-17 14:04:38 -0400

    agregue UpsertJugadorUseCase.kt

--- app/src/main/java/com/example/registrojugadores/domain/usecase/UpsertJugadorUseCase.kt
+++ app/src/main/java/com/example/registrojugadores/domain/usecase/UpsertJugadorUseCase.kt
@@ -8,17 +8,14 @@ class UpsertJugadorUseCase(
     private val repository: JugadorRepository
 ) {
     suspend operator fun invoke(jugador: Jugador): Result<Int> {
-        // Obtener todos los jugadores existentes para validar duplicados
         val existingJugadores = repository.observeJugadores()
-            .first() // Obtener el valor actual de la lista de jugadores
+            .first()
 
-        // Validar jugador
         val validationResult = validateJugadorUi(jugador.nombres, jugador.partidas.toString())
         if (!validationResult.isValid) {
             return Result.failure(IllegalArgumentException(validationResult.nombresError))
         }
 
-        // Guardar jugador usando el repositorio
         return runCatching { repository.upsert(jugador) }
     }
 }
