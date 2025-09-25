2a91a566c84535ef775c1c57d47cd8be7164e64e
jimmy057 <jimmypolanco2540@gmail.com>
2025-09-25 15:47:45 -0400

    modificacion una sola base de datos

--- app/src/main/java/com/example/registrojugadores/data/local/databases/AppDatabase.kt
+++ app/src/main/java/com/example/registrojugadores/data/local/databases/AppDatabase.kt
@@ -2,13 +2,24 @@ package com.example.registrojugadores.data.local.databases
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.registrojugadores.data.local.Converters
 import com.example.registrojugadores.data.local.dao.JugadorDao
 import com.example.registrojugadores.data.local.dao.PartidaDao
+import com.example.registrojugadores.data.local.dao.LogroDao
 import com.example.registrojugadores.data.local.entities.JugadorEntity
 import com.example.registrojugadores.data.local.entities.PartidaEntity
+import com.example.registrojugadores.data.local.entities.LogroEntity
 
-@Database(entities = [JugadorEntity::class, PartidaEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [JugadorEntity::class, PartidaEntity::class, LogroEntity::class],
+    version = 2,
+    exportSchema = false
+)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
+
     abstract fun jugadorDao(): JugadorDao
+    abstract fun logroDao(): LogroDao
     abstract fun partidaDao(): PartidaDao
 }
