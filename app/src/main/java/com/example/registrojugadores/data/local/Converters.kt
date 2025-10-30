a547a2fb3ae66f5156cd29623c5d019e8bb64030
jimmy057 <jimmypolanco2540@gmail.com>
2025-10-29 21:12:09 -0400

    modfificacion en el Converters.kt

--- app/src/main/java/com/example/registrojugadores/data/local/Converters.kt
+++ app/src/main/java/com/example/registrojugadores/data/local/Converters.kt
@@ -5,20 +5,34 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
 class Converters {
+
+    private val gson = Gson()
+
     @TypeConverter
-    fun toBoardList(boardString: String?): List<Int?>? {
-        if (boardString == null) return emptyList()
-        return try {
-            val type = object : TypeToken<List<Int?>>() {}.type
-            Gson().fromJson(boardString, type)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            emptyList()
-        }
+    fun fromLogrosList(list: List<String>?): String {
+        return gson.toJson(list ?: emptyList<String>())
     }
 
     @TypeConverter
-    fun fromBoardList(board: List<Int?>?): String? {
-        return Gson().toJson(board)
+    fun toLogrosList(value: String?): List<String> {
+        if (value.isNullOrEmpty()) return emptyList()
+        val type = object : TypeToken<List<String>>() {}.type
+        return gson.fromJson(value, type)
+    }
+
+    @TypeConverter
+    fun fromBoardList(board: List<Int?>?): String {
+        return gson.toJson(board ?: emptyList<Int?>())
+    }
+
+    @TypeConverter
+    fun toBoardList(boardString: String?): List<Int?> {
+        if (boardString.isNullOrEmpty()) return emptyList()
+        val type = object : TypeToken<List<Int?>>() {}.type
+        return gson.fromJson(boardString, type)
     }
 }
+
+
+
+
